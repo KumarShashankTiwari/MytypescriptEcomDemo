d6206b242eb2e06c78decbab8d28870e622d5562
unknown <shashanktiwari.kumar@gmail.com>
2022-02-27 01:17:23 +0530

    shahsnakj

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import {useState} from 'react';
-import { useQuery } from 'react-query';
-import { Drawer } from '@material-ui/core';
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer } from "@material-ui/core";
 
-import { LinearProgress } from '@material-ui/core';
-import { Grid } from '@material-ui/core';
-import { AddShoppingCart } from '@material-ui/icons';
- 
+import { LinearProgress } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
+//styles
+import { Wrapper } from "./App.styles";
+// Types
+export type CartItemType = {
+  id: number;
+  category: String;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products',getProducts);
+  console.table(data);
   return <div className="App">Start</div>;
 };
 
