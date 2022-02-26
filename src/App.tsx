ecfd5ed926a969c5ffdfffe0774dc3188e64a98c
unknown <shashanktiwari.kumar@gmail.com>
2022-02-27 00:52:40 +0530

    shshank

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { useQuery } from 'react-query';
+import { Drawer } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { LinearProgress } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+ 
+const App = () => {
+  return <div className="App">Start</div>;
+};
 
 export default App;
